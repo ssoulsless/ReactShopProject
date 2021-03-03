ed88be4071f0a25498a4022c0d971c02eba38103
silversoulsless <doodlezzzfobozzz@gmail.com>
2021-03-03 21:02:56 +0300

    gh-pages added

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 
 function Footer() {
 	return (
-		<footer className="page-footer deep-purple accent-5">
-			<div className="footer-copyright">
-				<div className="container">
-                    © 2021 Copyright Text
-					<a className="grey-text text-lighten-4 right" href="#!">Repo</a>
+		<footer className='page-footer deep-purple accent-5'>
+			<div className='footer-copyright'>
+				<div className='container'>
+					© 2021 Copyright Text
+					<a
+						className='grey-text text-lighten-4 right'
+						href='https://github.com/ssoulsless/ReactShopProject'
+						target='blank'>
+						Repo
+					</a>
 				</div>
 			</div>
 		</footer>
 	);
 }
-export {Footer};
+export { Footer };
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -2,15 +2,23 @@ import React from 'react';
 
 function Header() {
 	return (
-		<nav className="deep-purple accent-4">
-			<div className="nav-wrapper">
-				<a href="!#" className="brand-logo">React Shop</a>
-				<ul id="nav-mobile" className="right hide-on-med-and-down">
-					<li><a href="!#">Repo</a></li>
+		<nav className='deep-purple accent-4'>
+			<div className='nav-wrapper'>
+				<a href='/' className='brand-logo'>
+					React Shop
+				</a>
+				<ul id='nav-mobile' className='right hide-on-med-and-down'>
+					<li>
+						<a
+							href='https://github.com/ssoulsless/ReactShopProject'
+							target='blank'>
+							Repo
+						</a>
+					</li>
 				</ul>
 			</div>
 		</nav>
 	);
 }
 
-export {Header};
+export { Header };
